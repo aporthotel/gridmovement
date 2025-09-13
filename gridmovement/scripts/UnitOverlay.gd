ef865d18fc96041494b43d9739ff82b96d101390
aporthotel <aporthotel@gmail.com>
2025-09-14 01:26:52 +0700

    Delete gridmovement directory

--- gridmovement/scripts/UnitOverlay.gd
+++
@@ -1,9 +0,0 @@
-## Overlay for showing walkable cells
-## Uses TileMapLayer (Godot 4.4.1)
-class_name UnitOverlay
-extends TileMapLayer
-
-func draw_cells(cells: Array) -> void:
-	clear()
-	for cell in cells:
-		set_cell(Vector2i(cell), 0, Vector2i.ZERO)
